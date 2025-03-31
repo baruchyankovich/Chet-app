9a45329422a3642565b39a3e92936ffaf945c664
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-03-31 16:45:15 +0300

    1

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    res.send('Signup controller');
+}
+export const login= (req, res) => {
+    res.send('login controller');
+}
+export const logout = (req, res) => {
+    res.send('logout controller');
+}
