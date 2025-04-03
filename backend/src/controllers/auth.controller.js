d77140296d4c3db1fa72ed2a778b64479ce0ef72
Baruch Yankovitz <baruchyankovitz@gmail.com>
2025-04-03 14:26:02 +0300

    update signup funcsion 0.2

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,33 +1,61 @@
+import { User } from '../models/user.model.js';
 export const signup = async (req, res) => {
     const { name, email, password, phone } = req.body;
 
     try {
         if (!name || !name.trim()) {
-            return res.status(400).send('Name is required');
+            return res.status(400).json({ success: false, message: 'Name is required' });
         }
         if (!email || !email.trim()) {
-            return res.status(400).send('Email is required');
+            return res.status(400).json({ success: false, message: 'Email is required' });
         }
         if (!password || !password.trim()) {
-            return res.status(400).send('Password is required');
+            return res.status(400).json({ success: false, message: 'Password is required' });
         }
         if (!phone) {
-            return res.status(400).send('Phone is required');
+            return res.status(400).json({ success: false, message: 'Phone is required' });
         }
 
+        const existingUser = await User.findOne({ 
+            $or: [{ email }, { phone }] 
+        });
+
+        if (existingUser) {
+            if (!existingUser.isVerified) {
+                
+                if (existingUser.verificationTokenExpiresAt < Date.now()) {
+                    await User.deleteOne({ _id: existingUser._id });
+                    console.log(`Deleted unverified user: ${existingUser.email}`);
+                } else {
+                    return res.status(400).json({ success: false, message: "Please verify your email before signing up again." });
+                }
+            } else {
+                return res.status(400).json({ success: false, message: "User already exists" });
+            }
+        }
+
+        
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const generateVerificationToken = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+    
         const user = new User({
             name,
             email,
-            password,
+            password: hashedPassword,
             phone,
+            verificationToken: generateVerificationToken(),
+            verificationTokenExpiresAt: Date.now() + 20 * 60 * 1000, // 20 minutes 
         });
 
         await user.save();
-        res.send('User created successfully');
+        res.status(201).json({ success: true, message: 'User created successfully' });
+
     } catch (error) {
-        res.status(500).send(error.message);
+        res.status(500).json({ success: false, message: error.message });
     }
 };
+
 
 export const login= (req, res) => {
     res.send('login controller');
